a3b30a135feec4a9e50511346b0b9255ad2fa081
strawhatrag <sreeragbharathan791@gmail.com>
2024-07-27 17:54:06 +0530

    added counter max char

--- src/components/FeedBackForm.tsx
+++ src/components/FeedBackForm.tsx
@@ -1,10 +1,31 @@
+import { useState } from "react";
+import { MAX_CHARACTER } from './../lib/constants';
+
+
+
 export default function FeedBackForm() {
+  const [text, setText] = useState("");
+
+
+  const charCount = MAX_CHARACTER - text.length;
   return (
     <form className="form">
-      <label htmlFor="feedback-textarea">Enter your feedback here, remember to #hashtag the company</label>
-      <textarea id="feedback-textarea" placeholder="blah" spellCheck={false} ></textarea>
+      <textarea
+        value={text}
+        onChange={(event) => {
+          setText(event.target.value);
+        }}
+        id="feedback-textarea"
+        placeholder="blah"
+        spellCheck={false}
+      />
+
+      <label htmlFor="feedback-textarea">
+        Enter your feedback here, remember to #hashtag the company
+      </label>
+
       <div>
-        <p className="u-italic">150</p>
+        <p className="u-italic">{charCount}</p>
         <button>Submit</button>
       </div>
     </form>
--- src/components/Feedbacklist.tsx
+++ src/components/Feedbacklist.tsx
@@ -1,7 +1,25 @@
-
+import { TriangleUpIcon } from "@radix-ui/react-icons"
 
 export default function Feedbacklist() {
   return (
-    <ol className="feedback-list">Feedbacklist</ol>
-  )
+    <ol className="feedback-list">
+      <li className="feedback">
+        <button>
+          <TriangleUpIcon/>
+          <span>593</span></button>
+        <div>
+          <p>B</p>
+        </div>
+        <div>
+        <p>Cisco</p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti
+          eaque tempora ea accusantium, deserunt ullam!
+        </p>
+      </div>{" "}
+      <p>4d</p>
+      </li>
+    
+    </ol>
+  );
 }
