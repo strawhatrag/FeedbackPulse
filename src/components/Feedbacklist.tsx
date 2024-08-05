4ba219d3297ea55f3c27adf14b5c838eb8eff7cd
strawhatrag <sreeragbharathan791@gmail.com>
2024-08-05 18:19:42 +0530

    added spinner component

--- src/components/Feedbacklist.tsx
+++ src/components/Feedbacklist.tsx
@@ -1,19 +1,26 @@
 import FeedbackItem from "./FeedbackItem";
 import { useEffect, useState } from "react";
+import Spinner from './Spinner';
 
 export default function FeedbackList() {
   const [feedbackItems, setFeedbackItems] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
+    setIsLoading(true);
     fetch(
       "https://bytegrad.com/course-assets/projects/corpcomment/api/feedbacks"
     ).then((response) => {
       return response.json();
-    }).then((data) => {setFeedbackItems(data.feedbacks) });
+    }).then((data) => {
+      setFeedbackItems(data.feedbacks);
+      setIsLoading(false);
+     });
   }, []);
 
   return (
     <ol className="feedback-list">
+      {isLoading ? <Spinner/> : null}
       {feedbackItems.map((feedbackItem) => {
         return <FeedbackItem key={feedbackItem.id} feedbackItem={feedbackItem} />;
       })}
